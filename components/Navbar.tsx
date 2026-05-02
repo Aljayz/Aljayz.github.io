437471b6f4c81bb4999fd78d60a0f8f283dbc834
Aljayz <albertjaybarororosique@gmail.com>
2026-05-03 05:02:48 +0800

    Refactor Navbar logo component for improved styling and responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,13 +21,9 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="#home">
-            <Image
-              src={Aljayz}
-              alt="Aljayz logo"
-              width={130}
-              height={60}
-              // Adjust width/height to your preferred logo size
-            />
+            <div className="relative w-30 h-20">
+                <Image src={Aljayz} alt="Aljayz logo" fill className="object-contain" />
+            </div>
           </a>
           {/* Desktop menu */}
           <div className="hidden md:flex space-x-6">
